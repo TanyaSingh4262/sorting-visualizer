ce7a80c1ba872289a444a3c45cc325585696700b
TanyaSingh4262 <tanyasingh3118@gmail.com>
2024-07-21 01:39:38 +0530

    Commit message describing the changes

---
+++ src/components/Form.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+
+import {
+
+    FormControl,
+    FormLabel,
+    RadioGroup,
+    FormControlLabel,
+    Radio,
+  } from '@mui/material';
+  
+export default function Form({
+	formLabel,
+	values,
+	labels,
+	currentValue,
+	onChange,
+}) {
+	return (
+		<div className='card container-small'>
+			<FormControl>
+				<FormLabel>{formLabel}</FormLabel>
+				<RadioGroup value={currentValue} onChange={onChange}>
+					{values.map((value, index) => {
+						return (
+							<FormControlLabel
+								key={`${value}_${index}`}
+								value={value}
+								control={<Radio />}
+                                label={labels[index]}
+							/>
+						);
+					})}
+				</RadioGroup>
+			</FormControl>
+		</div>
+	);
+}
